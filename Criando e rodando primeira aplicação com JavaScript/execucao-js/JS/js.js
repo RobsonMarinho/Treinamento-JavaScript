185acf53fb1e631b77c290241aced7e7781e4acb
Robson Marinho <robson_luz15@hotmail.com>
2019-01-28 22:30:21 -0300

    estruturando o JS

---
+++ Criando e rodando primeira aplicação com JavaScript/execucao-js/JS/js.js
@@ -0,0 +1,11 @@
+//Declaração de variáveis
+var nome = "Ricardo";
+var sobreNome = "Aguiar Luz";
+
+console.log("Instrução solta em um arquivo JS");
+
+function exibirNomeConsole() {
+    console.log("exibir nome: " + nome);
+}
+
+exibirNomeConsole();
