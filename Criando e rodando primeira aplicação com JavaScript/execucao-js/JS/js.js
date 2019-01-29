f12df166e1b0442efc9fc6202c3cfe54a405895d
Robson Marinho <robson_luz15@hotmail.com>
2019-01-28 22:41:54 -0300

    Execucao de codigo JS com evento onload

--- Criando e rodando primeira aplicação com JavaScript/execucao-js/JS/js.js
+++ Criando e rodando primeira aplicação com JavaScript/execucao-js/JS/js.js
@@ -7,5 +7,10 @@ console.log("Instrução solta em um arquivo JS");
 function exibirNomeConsole() {
     console.log("exibir nome: " + nome);
 }
-
+//chama função
 exibirNomeConsole();
+
+//Função exbir nome completo
+function exibirNomeCompleto() {
+    console.log("Programador, nome completo: " + nome + " " + sobrenome);
+}
