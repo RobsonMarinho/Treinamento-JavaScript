c60a94024e59fb06bb1df5c03eefd8282a89ef27
Robson Marinho <robson_luz15@hotmail.com>
2019-03-06 21:15:19 -0300

    Inserindo comentários no JS

--- Estrutura léxica - sintaxe do JavaScript e declaração de variáveis/js/js.js
+++ Estrutura léxica - sintaxe do JavaScript e declaração de variáveis/js/js.js
@@ -1,6 +1,12 @@
 //Regras de escrita
+
+//Declaração de variáveis
 var nome = "Pedro";
 var idade = 28;
 var pretensaoSalarial = 3277.22;
 var valeRefeicao = true;
 
+function calcTributo() {
+    //code...
+
+}
