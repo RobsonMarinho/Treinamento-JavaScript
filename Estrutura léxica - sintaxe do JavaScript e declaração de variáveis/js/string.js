dc4dda39a0bcf33d8f052ff30359be28c5f9da0d
Robson Marinho <robson_luz15@hotmail.com>
2019-03-09 23:59:59 -0300

    Tipo de dado String

---
+++ Estrutura léxica - sintaxe do JavaScript e declaração de variáveis/js/string.js
@@ -0,0 +1,10 @@
+var nome = "Prof. Madson ";
+var sobreNome = 'Aguiar Rodrigues';
+var nomeCompleto = nome + sobreNome;
+var vazia = "";
+
+
+console.log("Nome completo: " + nomeCompleto + vazia);
+
+var numeroString = "20" + 10;
+var numeroStringInv = 10 + "50";
