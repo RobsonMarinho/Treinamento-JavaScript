b7a5bd11b5b0a2b80918d81a13edadc48ddeb8ec
Robson Marinho <robson_luz15@hotmail.com>
2019-03-10 23:53:30 -0300

    Finalizando tipo de dado String

--- Estrutura léxica - sintaxe do JavaScript e declaração de variáveis/js/string.js
+++ Estrutura léxica - sintaxe do JavaScript e declaração de variáveis/js/string.js
@@ -3,8 +3,17 @@ var sobreNome = 'Aguiar Rodrigues';
 var nomeCompleto = nome + sobreNome;
 var vazia = "";
 
-
 console.log("Nome completo: " + nomeCompleto + vazia);
 
 var numeroString = "20" + 10;
 var numeroStringInv = 10 + "50";
+
+console.log("valor: " + numeroString + " - " + numeroStringInv);
+
+var stringVar = "string texti";
+var numeroVar = 10;
+
+console.log("tipo1: " + typeof stringVar);
+console.log("tipo2: " + typeof numeroVar);
+
+console.log("Tamanho string: " + stringVar.length);
