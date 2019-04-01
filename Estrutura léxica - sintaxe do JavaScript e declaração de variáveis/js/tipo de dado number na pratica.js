84acfa44d60e55c96a4159a740a36aafeb1b9200
Robson Marinho <robson_luz15@hotmail.com>
2019-03-31 23:58:38 -0300

    Tipo de dado number

---
+++ Estrutura léxica - sintaxe do JavaScript e declaração de variáveis/js/tipo de dado number na pratica.js
@@ -0,0 +1,15 @@
+var idade = 28;
+console.log(idade);
+
+console.log(typeof idade);
+
+var precoFeijao = 2.89;
+
+console.log(precoFeijao);
+console.log(typeof precoFeijao);
+
+var valor = 23 + 33;
+console.log(valor);
+
+var valor = +"23" + 33;
+console.log(valor);
