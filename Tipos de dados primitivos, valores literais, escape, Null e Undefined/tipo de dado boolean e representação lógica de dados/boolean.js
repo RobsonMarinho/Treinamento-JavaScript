79da3b5675ee75e630f88ae97e4b8cf5afd8ea26
Robson Marinho <robson_luz15@hotmail.com>
2019-04-02 00:59:50 -0300

    tipo de dado boolean

---
+++ Tipos de dados primitivos, valores literais, escape, Null e Undefined/tipo de dado boolean e representação lógica de dados/boolean.js
@@ -0,0 +1,7 @@
+var contaStatus = false;
+console.log("Status da conta:  ", contaStatus);
+
+var contaStatus = true;
+HTMLFormControlsCollection.log("Status da conta: ", contaStatus);
+
+console.log(typeof contaStatus);
