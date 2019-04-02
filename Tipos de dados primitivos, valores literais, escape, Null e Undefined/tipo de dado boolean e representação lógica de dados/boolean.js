d62bddebaf639903155e21d645a9e1e461a16b00
Robson Marinho <robson_luz15@hotmail.com>
2019-04-02 01:17:40 -0300

    48. Tipo de dado Boolean e representação lógica de dados

--- Tipos de dados primitivos, valores literais, escape, Null e Undefined/tipo de dado boolean e representação lógica de dados/boolean.js
+++ Tipos de dados primitivos, valores literais, escape, Null e Undefined/tipo de dado boolean e representação lógica de dados/boolean.js
@@ -2,6 +2,35 @@ var contaStatus = false;
 console.log("Status da conta:  ", contaStatus);
 
 var contaStatus = true;
-HTMLFormControlsCollection.log("Status da conta: ", contaStatus);
+console.log("Status da conta: ", contaStatus);
 
+//Verifica o tipo de dado via "typeof"
 console.log(typeof contaStatus);
+
+if(contaStatus == true){
+    console.log("Enviar email!");
+}else{
+    console.log("Ative sua conta!");
+}
+
+console.log(!contaStatus);
+
+var nome = "madson";
+var preço = 22.8;
+var vazio = "";
+
+console.log(!!nome);
+console.log(!!preco);
+console.log(!!vazio);
+console.log("-----");
+
+//6 valores / objetos
+console.log(!!""); //String vazio FALSE
+console.log(!!0); //Sempre false
+console.log(!!-0); //Sempre false
+console.log(!!null); //Sempre false
+console.log(underfined); //Sempre false
+console.log(NaN);  //Sempre false
+console.log(!!Array);//Sempre true
+console.log(!!Object);//Sempre true
+console.log(!!Function);//Sempre true
