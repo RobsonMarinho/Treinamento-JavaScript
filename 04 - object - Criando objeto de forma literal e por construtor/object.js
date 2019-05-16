f5823325a469fc70d45449ba4f5e0cd46c857612
Robson Marinho <robson_luz15@hotmail.com>
2019-05-16 00:26:35 -0300

    Criando objeto de forma literal e por construtor e Organizando arquivos do projeto no diretório

---
+++ 04 - object - Criando objeto de forma literal e por construtor/object.js
@@ -0,0 +1,16 @@
+//literal
+var itens = {}
+var itens2 = {nome: "refrigerante", preco: 22, ativo: true, detalhes: {detalhe: '...'}};
+
+//Construtor
+var pessoa = new Object();
+pessoa.nome = "João";
+pessoa.idade = 44;
+
+//Accessar
+console.log(itens2.nome);
+console.log(itens2.preco);
+console.log(itens2['ativo']);
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(itens2.detalhes.detalhe);
