0bad88a74d44157e7e182305c5bb48001f6cfe5d
Robson Marinho <robson_luz15@hotmail.com>
2019-06-02 02:59:20 -0300

    Conversão entre tipos de dados

---
+++ 05 - Objeto window, escopo de variáveis, conversão de dados e Objetos Wrapper/js/conversao-dados.js
@@ -0,0 +1,11 @@
+//Conversão automática
+
+console.log(23 + ' trinta e cinco');
+console.log(55 + "5");
+console.log(10 / "2");  //Identificou a operação de divisão
+console.log(3 * "2");   //Identificou a operação de multiplicação
+console.log("3" / {valor:5});
+
+//Conversão de forma explícita por construtor
+console.log(Number("23"));
+console.log(typeof Number("23"));   //Identifica o tipo da variável
