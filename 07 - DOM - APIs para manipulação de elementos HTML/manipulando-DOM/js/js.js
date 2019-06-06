507bef610a0888535190376043f204a1d64deec4
Robson Marinho <robson_luz15@hotmail.com>
2019-06-06 00:14:28 -0300

    Selecionando elementos HTML via seletores CSS

--- 07 - DOM - APIs para manipulação de elementos HTML/manipulando-DOM/js/js.js
+++ 07 - DOM - APIs para manipulação de elementos HTML/manipulando-DOM/js/js.js
@@ -57,3 +57,5 @@ function selecionarCamporRadios() {
 var checkeds = document.getElementsByClassName("checkbox");
 
 var selects = document.getElementsByTagName("select");
+
+var elementos = document.querySelectorAll("input[type=text]");
